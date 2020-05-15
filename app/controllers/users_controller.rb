3e384d836b477608aa8d4332be2ea23786222f64
StevenDRiggs <sfriggs21@gmail.com>
2020-05-15 03:07:36 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
 
+  get '/users/:id' do
+    @user = User.find_by_id(params[:id])
+
+    erb :'users/show'
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+
+  def self.find_by_slug(slug)
+    self.all.select {|user| user.slug == slug}.first
+  end
+
+
+  def slug
+    self.username.gsub(' ', '-')
+  end
+
 end
