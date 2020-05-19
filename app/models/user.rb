c54b594aaaa8da093e63c05c7c960611f1501ac9
jkatem <jenmiao@gmail.com>
2020-05-18 18:37:42 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def self.find_by_slug(slug)
+    self.all.select {|user| user.slug == slug}.first
+  end
+
+
+  def slug
+    self.username.gsub(' ', '-')
+  end
 end
